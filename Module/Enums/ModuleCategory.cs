22c91bd3385ceedeffaa11f86dc6fa5c4344f3a4
AtmoOmen <atmoomen2004@outlook.com>
2026-06-08 18:12:14 +0800

    初步模块分类调整

--- Module/Enums/ModuleCategory.cs
+++ Module/Enums/ModuleCategory.cs
@@ -1,17 +1,23 @@
 namespace DailyRoutines.Common.Module.Enums;
 
+// 一等分类较为笼统, 二等分类较为具体
+// 原则上, 如果一个模块既能被分入一等分类又能被分入二等分类, 则归属于二等分类
 public enum ModuleCategory
 {
     None,
+    
+    // 一等分类
     General,
     System,
-    Action,
+    Interface,
     Combat,
-    UIOptimization,
-    UIOperation,
-    GoldSaucer,
-    Notice,
-    Assist,
+    Notification,
     Script,
-    Recruitment
+    Assist,
+    
+    // 二等分类
+    Duty,
+    Recruitment,
+    Action,
+    GoldSaucer,
 }
